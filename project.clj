b36b2ad4d123b00c94c2a50c95b71baba841d3ff
Roland Sadowski <szabla@gmail.com>
2014-11-05 02:36:37 +0000

    update details in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject rosado.fun "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Option|None type for Clojure/ClojureScript."
+  :url "https://github.com/rosado/rosado.fun"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
